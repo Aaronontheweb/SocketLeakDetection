d5c26025261de50af88d5120903b0bd88ebb2692
Aaron Stannard <aaron@petabridge.com>
2019-04-16 17:48:56 -0500

    added cluster quarantine reproduction

--- src/SocketLeakDetection.ClusterQuarantine.Demo/Program.cs
+++ src/SocketLeakDetection.ClusterQuarantine.Demo/Program.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using Akka.Actor;
+using Akka.Cluster;
 using Akka.Configuration;
+using Akka.Remote;
 using Akka.Routing;
+using Akka.Util.Internal;
 
 namespace SocketLeakDetection.ClusterQuarantine.Demo
 {
@@ -34,6 +40,7 @@ namespace SocketLeakDetection.ClusterQuarantine.Demo
 			            }
 		            }
                 }
+                akka.cluster.seed-nodes = [""akka.tcp://quarantine-test@127.0.0.1:9444""]
             ";
         }
 
@@ -47,9 +54,36 @@ namespace SocketLeakDetection.ClusterQuarantine.Demo
             return node;
         }
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            
+            // launch seed node
+            var seed = StartNode(9444);
+            var leakDetector = seed.ActorOf(Props.Create(() => new TcpPortUseSupervisor(new[]{ IPAddress.Loopback })), "portMonitor");
+
+            // start node that will be quarantined
+            var quarantineNode = StartNode(9555);
+            var node2Addr = Cluster.Get(quarantineNode).SelfAddress;
+            var uid = AddressUidExtension.Uid(quarantineNode);
+
+            //var peanutGallery = Enumerable.Repeat(1, 3).Select(x => StartNode(0)).ToList();
+
+            Func<int, bool> checkMembers = i => Cluster.Get(seed).State.Members.Count == i;
+
+            seed.Log.Info("Waiting for members to join...");
+            while (!checkMembers(2))
+            {
+                await Task.Delay(TimeSpan.FromSeconds(2));
+            }
+            seed.Log.Info("Cluster up.");
+
+            RarpFor(seed).Quarantine(node2Addr, uid);
+
+            seed.WhenTerminated.Wait();
+        }
+
+        static RemoteActorRefProvider RarpFor(ActorSystem system)
+        {
+            return system.AsInstanceOf<ExtendedActorSystem>().Provider.AsInstanceOf<RemoteActorRefProvider>();
         }
     }
 }
